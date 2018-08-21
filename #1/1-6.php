5e89375e36d0321a181bf9f8a086754f5e7bc1a8
Paul Kozlov <g1779@mail.ru>
2018-08-21 21:08:57 +0300

    2

--- #1/1-6.php
+++ #1/1-6.php
@@ -1,2 +1,16 @@
 <?php
-echo "<table"
+echo "<table border='1' cellpadding='2'>";
+for ($i = 1; $i < 11; $i++) {
+    echo "<tr>";
+    for ($j = 1; $j < 11; $j++) {
+        $z = $i * $j;
+        if ($z % 2 == 0) {
+            $style = "style=\" background-color:red;\"";
+        } else {
+            $style="";
+        }
+        echo "<td".$style.">".$i." x ".$j." = ".$z."</td>";
+    }
+    echo "</tr>";
+}
+echo "<table>";
